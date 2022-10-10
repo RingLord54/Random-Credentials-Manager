919f01afc3eb956b40c656de86780336f244cb88
RingLord54 <andrew.moneypenny@btinternet.com>
2022-10-10 19:48:49 +0100

    Main Files Added
    Files copied over from main project folder

---
+++ IronXL_Utilities.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronXL;
+
+namespace Random_Credentials_Generator
+{
+    public class IronXL_Utilities
+    {
+
+
+        public static string GetAddressRange(int column)
+        {
+            String value = "";
+            switch (column)
+            {
+                case 0: value = "A"; break;
+                case 1: value = "B"; break;
+                default: return "Failed Lookup";
+            }
+            String address = value + "2:" + value + "1000";
+            return address;
+        }
+
+
+        public static string GetBlankCell(WorkSheet sheet, int column)
+        {
+            String cellBlank = "";
+            String address = GetAddressRange(column);
+
+            if (address != "Failed Lookup")
+            {
+                foreach (var cell in sheet[address])
+                {
+                    if (cell.Text == "")
+                    {
+                        cellBlank = cell.AddressString;
+                        break;
+                    }
+                }
+                return cellBlank;
+            }
+            return "Failed Lookup";
+
+        }
+
+
+        public static void AddName(WorkBook workbook, WorkSheet sheet, string name, int column)
+        {
+            string address = GetAddressRange(column);
+
+            if (address != "Failed Lookup")
+            {
+                foreach (var cell in sheet[address])
+                {
+                    if (cell.Text == name)
+                    {
+                        Console.WriteLine("Name unsuccessfully added to the list!");
+                        return;
+                    }
+                }
+                string cellEdit = GetBlankCell(sheet, column);
+
+                sheet[cellEdit].Value = name;
+
+                workbook.SaveAs("Names.xlsx");
+
+                Console.WriteLine("Name successfully added to the list!");
+
+                return;
+            }
+            Console.WriteLine("Incorrect Column value entered");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronXL;
+
+namespace Random_Credentials_Generator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            WorkBook workbook = WorkBook.Load("Names.xlsx");
+            WorkSheet sheet = workbook.GetWorkSheet("Sheet1");
+
+            Console.WriteLine("Name Generator Application");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("\nWelcome to the Name Generator Application!\n");
+            Console.Write("Would you like to:\n- Add a name to the list of names that can be selected (Enter 0)\n- Randomly Generate a Name (Enter 1)\n");
+            Console.Write("\nInput: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nYour choice: " + num + "\n");
+
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Thank you for using the Name Generator Application!");
+
+            //IronXL_Utilities.AddName(workbook, sheet, "Bonny", 1);
+        }
+    }
+}
