2126253be829d8ad79bd04e71844ce2a0472e66d
RingLord54 <andrew.moneypenny@btinternet.com>
2022-10-13 21:58:32 +0100

    Menu class commented

--- Menu.cs
+++ Menu.cs
@@ -14,15 +14,27 @@ namespace Random_Credentials_Generator
     /// </summary>
     internal class Menu
     {
-        private string title;
-        private string[] options;
+        private string title;       // Menu Title
+        private string[] options;   // Menu Options
 
+
+        /// <summary>
+        /// The menu class' constructor
+        /// </summary>
+        /// <param name="title">Menu Title</param>
+        /// <param name="options">Menu Options</param>
         public Menu(string title, string[] options)
         {
             this.title = title;
             this.options = options;
         }
 
+
+        /// <summary>
+        /// The purpose of this function is to display the title and the options
+        /// of the menu provided in an appealing and organised way, to make it seem
+        /// more professional to the user.
+        /// </summary>
         private void Display()
         {
             Console.WriteLine(title);
@@ -38,19 +50,35 @@ namespace Random_Credentials_Generator
             Console.WriteLine();
         }
 
+
+        /// <summary>
+        /// The purpose of this function is to display the menu's title and options and
+        /// then provide the User the option to input a selection to then be processed
+        /// in order to carry out an operation. A try catch is implemented to ensure integer
+        /// values only are entered.
+        /// </summary>
+        /// <returns>The number value of the selected menu option</returns>
         public int GetUserChoice()
         {
             int value = 0;
             Display();
             do {
+
+                // Try to get a valid user selection
                 try {
                     Console.WriteLine("Enter Selection: ");
                     value = Convert.ToInt32(Console.ReadLine());
+
+                    // If the entered value is valid break from the selection loop
                     break;
                 }
+                // If the input isn't an integer value
                 catch(Exception ex) {
+
+                    // Display a message notifying the User of what went wrong
                     Console.WriteLine("Invalid selection entered. Please try again.");
                 }
+            // Keep going until a valid selection is made by the user
             } while (true);
 
             return value;
